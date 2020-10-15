7c9a80e783cb1bdb8aa1de9e804f0d3e54a61b14
Landon Yarrington <jly.dev.36963@gmail.com>
2020-10-15 01:27:06 -0600

    go standard-packages notes

--- go--basics/standard-packages/main.go
+++ go--basics/standard-packages/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"runtime"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -23,6 +25,18 @@ func main() {
 	// strings
 	fmt.Println(strings.ToUpper("strings"))
 	useStrings()
+
+	// json
+	fmt.Println(strings.ToUpper("json"))
+	useJson()
+
+	// time
+	fmt.Println(strings.ToUpper("time"))
+	useTime()
+
+	// path
+	fmt.Println(strings.ToUpper("path"))
+	usePath()
 }
 
 // ---
@@ -152,4 +166,109 @@ func useStrings() {
 		"upperCase", upperCase,
 		"trimmed", trimmed,
 	)
+}
+
+// ---
+// json
+// ---
+
+type Person struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+func useJson() {
+
+	// struct -> []byte
+	bytes, _ := json.Marshal(Person{
+		FirstName: "Kakashi",
+		LastName:  "Hatake",
+	})
+	// []byte -> struct
+	var person Person
+	err := json.Unmarshal(bytes, &person)
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+	// print struct
+	fmt.Println(fmt.Sprintf("%+v", person))
+	// pretty print
+	formattedBytes, _ := json.MarshalIndent(Person{
+		FirstName: "Kakashi",
+		LastName:  "Hatake",
+	}, "", "  ")
+	fmt.Println(fmt.Sprintf("%s", formattedBytes))
+	// valid json
+	isValid := json.Valid(formattedBytes) // boolean
+	fmt.Println("isValid", isValid)
+}
+
+// ---
+// time
+// ---
+
+func useTime() {
+	// Date
+	date := time.Date(2020, 10, 11, 12, 0, 0, 0, time.UTC)
+	// Now
+	now := time.Now()
+	// Hour (Nanosecond, Second, Minute, Hour, Day, Weekday, Month, Year, YearDay)
+	hour := time.Now().Hour()
+	// Since
+	beginning := time.Now()
+	sinceDuration := time.Since(beginning) // duration
+	// Until
+	untilDuration := time.Until(time.Now().Add(time.Hour)) // duration
+	// ParseDuration
+	parsedDuration, _ := time.ParseDuration("4h30m")
+	// Hours (also Nanoseconds, Microseconds, Milliseconds, Seconds, Minutes)
+	hours := parsedDuration.Hours()
+	// Add (time.Second, time.Minute, time.Hour)
+	tomorrow := time.Now().Add(time.Hour * 24) // add time to a date
+	// Sub
+	start := time.Now()
+	end := time.Now()
+	difference := end.Sub(start) // difference between dates
+	// AddDate
+	newDate := time.Now().AddDate(3, 2, 1) // add 3 years, 2 months, 1 day
+	// Before
+	before := time.Now().Before(time.Now().Add(time.Hour)) // now is before now + 1hr
+	// After
+	after := time.Now().After(time.Now().Add(time.Hour * -1)) // now is after now - 1hr
+	// Format
+	formattedDate := time.Now().Format(time.RFC3339)
+	// In
+	timeSomewhereElse := time.Now().In(time.UTC)
+	// Unix
+	dateFromUnixTimestamp := time.Unix(1602745919, 0) // (s, ns) => date
+	// Round
+	pd, _ := time.ParseDuration("1h15m10s")
+	roundedDuration := pd.Round(time.Minute)
+
+	fmt.Println(
+		"date", date,
+		"now", now,
+		"hour", hour,
+		"sinceDuration", sinceDuration,
+		"untilDuration", untilDuration,
+		"parsedDuration", parsedDuration,
+		"hours", hours,
+		"tomorrow", tomorrow,
+		"difference", difference,
+		"newDate", newDate,
+		"before", before,
+		"after", after,
+		"formattedDate", formattedDate,
+		"timeSomewhereElse", timeSomewhereElse,
+		"dateFromUnixTimestamp", dateFromUnixTimestamp,
+		"roundedDuration", roundedDuration,
+	)
+}
+
+// ---
+// path
+// ---
+
+func usePath() {
+
 }
