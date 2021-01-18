fad1a65b49278e2b83199f5a4ebb7403486484a1
Landon Yarrington <jly.dev.36963@gmail.com>
2021-01-18 10:03:02 -0700

    arrow

--- js--node/sharp.js
+++ js--node/sharp.js
@@ -5,7 +5,7 @@
 // install
 `
 npm i --save sharp
-`
+`;
 
 // IMPORTANT -- architecture/platform of Node.js used for install must be the same as runtime
 
@@ -17,26 +17,24 @@ npm i --save sharp
 // ------------
 
 // promise returned when callback not provided
-  // .toFile(fn, (err, info) => {})
-  // .toFile(fn).then(info => {}).catch(err => {})
+// .toFile(fn, (err, info) => {})
+// .toFile(fn).then(info => {}).catch(err => {})
 
 // import
-const sharp = require('sharp');
+const sharp = require("sharp");
 // filenames
-const fnInput = './files/input.jpg';
-const fnOutput = './files/output.jpg';
+const fnInput = "./files/input.jpg";
+const fnOutput = "./files/output.jpg";
 // async await & .catch
 const info = await sharp(fnInput)
   .resize(300)
   .toFile(fnOutput)
-  .catch(err => console.log(err))
+  .catch((err) => console.log(err));
 console.log(info);
 // async await & try catch
 try {
-  const info = await sharp(fnInput)
-    .resize(300)
-    .toFile(fnOutput);
-  console.log(info); // 
+  const info = await sharp(fnInput).resize(300).toFile(fnOutput);
+  console.log(info); //
 } catch (err) {
   console.log(err);
 }
@@ -48,75 +46,72 @@ try {
 // metadata properties -- format, size, width, height, exif, etc
 
 // import
-const sharp = require('sharp');
+const sharp = require("sharp");
 // filenames
-const fnInput = './files/input.jpg';
-const fnOutput = './files/output.jpg';
+const fnInput = "./files/input.jpg";
+const fnOutput = "./files/output.jpg";
 // read image
 const image = sharp(fnInput);
 // get metadata
-const metadata = await image.metadata()
-  .catch(err => console.log(err))
+const metadata = await image.metadata().catch((err) => console.log(err));
 // use metadata during processing
 const info = await image
   .resize(Math.round(metadata.width / 2)) // resize
   .toFile(fnOutput)
-  .catch(err => console.log(err))
+  .catch((err) => console.log(err));
 console.log(info);
 
 // ------------
-// stats 
+// stats
 // ------------
 
 // stats properties -- channels (array of channel statistics objects), isOpaque, entropy
 // channels properties -- min, max, sum, squareSum, mean, stdev, minX, minY, maxX, maxY
 
 // import
-const sharp = require('sharp');
+const sharp = require("sharp");
 // filenames
-const fnInput = './files/input.jpg';
+const fnInput = "./files/input.jpg";
 // read image
 const image = sharp(fnInput);
 // get stats
-const stats = await image.stats()
-  .catch(err => console.log(err));
+const stats = await image.stats().catch((err) => console.log(err));
 console.log(stats);
-
 
 // ------------
 // output options
 // ------------
 
-// toFile 
-  // supported: jpeg, png, webp, tiff, dzi, v (libvips format)
-  // if format not set, it will be inferred from the extension
-  // metadata will be removed (unless 'withMetadata')
+// toFile
+// supported: jpeg, png, webp, tiff, dzi, v (libvips format)
+// if format not set, it will be inferred from the extension
+// metadata will be removed (unless 'withMetadata')
 // toBuffer
-  // write output to a buffer
-  // supported: jpeg, png, webp, tiff, and raw
-  // if format not set, it will be inferred from input type (except SVG and GIF --> PNG)
-  // 
+// write output to a buffer
+// supported: jpeg, png, webp, tiff, and raw
+// if format not set, it will be inferred from input type (except SVG and GIF --> PNG)
+//
 // withMetadata
-  // metadata will NOT be removed for output.
+// metadata will NOT be removed for output.
 // tile
-  // tile-based deep zoom (image pyramid) output. 
-  // set format explicitly, use `.zip` or `.szi` output extenxion
+// tile-based deep zoom (image pyramid) output.
+// set format explicitly, use `.zip` or `.szi` output extenxion
 
 // import
-const sharp = require('sharp');
+const sharp = require("sharp");
 // filenames
-const fnInput = './files/input.jpg';
-const fnOutput = './files/output.jpg';
+const fnInput = "./files/input.jpg";
+const fnOutput = "./files/output.jpg";
 // read image
 const image = sharp(fnInput);
-// process image 
+// process image
 const info = await image
   .resize(500)
   .withMetaData()
   .jpeg() // set format // jpeg, png, webp, tiff, etc
   .toFile(fnOutput)
-  .catch(err => console.log(err))
-console.log(info)
+  .catch((err) => console.log(err));
+console.log(info);
 
 // toBuffer
 
@@ -127,22 +122,22 @@ console.log(info)
 // ------------
 
 // import
-const sharp = require('sharp');
+const sharp = require("sharp");
 // filenames
-const fnInput = './files/input.jpg';
-const fnOutput = './files/output.jpg';
+const fnInput = "./files/input.jpg";
+const fnOutput = "./files/output.jpg";
 // read image
 const image = sharp(fnInput);
-// process image 
+// process image
 const info = await image
-  .resize({ 
-    width: 200, 
-    height: 200, 
-    fit: 'cover' // cover, contain, fill, inside, outside
+  .resize({
+    width: 200,
+    height: 200,
+    fit: "cover", // cover, contain, fill, inside, outside
   })
   .toFile(fnOutput)
-  .catch(err => console.log(err))
-console.log(info)
+  .catch((err) => console.log(err));
+console.log(info);
 
 // ------------
 // extend
@@ -161,13 +156,13 @@ console.log(info)
 // ------------
 
 // import
-const sharp = require('sharp');
+const sharp = require("sharp");
 // filenames
-const fnInput = './files/input.jpg';
-const fnOutput = './files/output.jpg';
+const fnInput = "./files/input.jpg";
+const fnOutput = "./files/output.jpg";
 // read image
 const image = sharp(fnInput);
-// process image 
+// process image
 const info = await image
   .rotate(90) // rotate (in degrees)
   .flip() // flip about the vertical y-axis (always occurs after rotation) (removes EXIF orientation)
@@ -179,17 +174,17 @@ const info = await image
   .gamma() // gamma correction. params: gamma (value between 1.0 and 3.0, default 2.2)
   .negate() // produce negative of image
   .normalise() // enhance output image contrast by stretching its luminance to cover the full dynamic range
-  .convolve({ width: 3, height: 3, kernel: [0,0,0,0,1,0,0,0,0]}) // convolve with specified kernel
+  .convolve({ width: 3, height: 3, kernel: [0, 0, 0, 0, 1, 0, 0, 0, 0] }) // convolve with specified kernel
   .threshold(128) // x >= threshold ? white : black
   .linear(1.0, 0.0) // transformation like `a*x + b`
   .modulate({
     brightness: 2, // increase lightness by a factor of 2
     hue: 180, // hue-rotate by 180 degrees
-    saturation: .5 // decrease saturation by a factor of .5
+    saturation: 0.5, // decrease saturation by a factor of .5
   })
   .toFile(fnOutput)
-  .catch(err => console.log(err))
-console.log(info)
+  .catch((err) => console.log(err));
+console.log(info);
 
 // ------------
 // colour manipulation
@@ -208,19 +203,23 @@ console.log(info)
 // ------------
 
 // import
-const sharp = require('sharp');
+const sharp = require("sharp");
 // use callback
 sharp(inputBuffer)
   .resize(320, 240)
-  .toFile('output.webp', (err, info) => console.log(err, info));
+  .toFile("output.webp", (err, info) => console.log(err, info));
 
 // use promise
-sharp('input.jpg')
+sharp("input.jpg")
   .rotate()
   .resize(200)
   .toBuffer()
-  .then(data => { console.log(data) })
-  .catch(err => { console.log(err) });
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // use async await
 const semiTransparentRedPng = await sharp({
@@ -228,8 +227,8 @@ const semiTransparentRedPng = await sharp({
     width: 48,
     height: 48,
     channels: 4,
-    background: { r: 255, g: 0, b: 0, alpha: 0.5 }
-  }
+    background: { r: 255, g: 0, b: 0, alpha: 0.5 },
+  },
 })
   .png()
   .toBuffer();
@@ -239,78 +238,54 @@ const roundedCorners = Buffer.from(
   '<svg><rect x="0" y="0" width="200" height="200" rx="50" ry="50"/></svg>'
 );
 
-const roundedCornerResizer =
-  sharp()
-    .resize(200, 200)
-    .composite([{
+const roundedCornerResizer = sharp()
+  .resize(200, 200)
+  .composite([
+    {
       input: roundedCorners,
-      blend: 'dest-in'
-    }])
-    .png();
+      blend: "dest-in",
+    },
+  ])
+  .png();
 
-readableStream
-  .pipe(roundedCornerResizer)
-  .pipe(writableStream);
-
-
+readableStream.pipe(roundedCornerResizer).pipe(writableStream);
 
 // ------------
 //
 // ------------
 
-
+// ------------
+//
+// ------------
 
 // ------------
 //
 // ------------
 
-
+// ------------
+//
+// ------------
 
 // ------------
 //
 // ------------
 
-
+// ------------
+//
+// ------------
 
 // ------------
 //
 // ------------
 
-
+// ------------
+//
+// ------------
 
 // ------------
 //
 // ------------
 
-
-
 // ------------
 //
 // ------------
-
-
-
-// ------------
-//
-// ------------
-
-
-
-// ------------
-//
-// ------------
-
-
-
-// ------------
-//
-// ------------
-
-
-
-// ------------
-//
-// ------------
-
-
-
--- py/arrow-notes.py
+++ py/arrow-notes.py
@@ -1,31 +1,72 @@
+# ---
+# arrow
+# ---
+
+# install
+# pip install arrow
+
+# docs
+# https://arrow.readthedocs.io/en/stable/
+
+# detailed api
+# https://arrow.readthedocs.io/en/stable/#api-guide
+
+# timezones
+# https://github.com/arrow-py/arrow/blob/master/arrow/locales.py
+
+
+# ---
 # imports
+# ---
 
-
-import sys, os, json
 import arrow
+import json
+from datetime import datetime, timezone
 
+# ---
 # arrow basics
+# ---
 
-now = arrow.now()
-first_day_of_year = arrow.now().floor('year')
-dates_generator = arrow.Arrow.range('day', first_day_of_year, now)
-dates = list(map(
-    lambda x: x.format('YYYY-MM-DD'),
-    list(dates_generator)
-))
+examples = {
+    # generate
+    'get': arrow.get(),
+    'get_from_datetime': arrow.get(datetime.now(timezone.utc)),
+    'utc': arrow.utcnow(),
+    'clone': arrow.utcnow().clone()
+    # format
+    'format_default': arrow.utcnow().format(),
+    'format': arrow.utcnow().format('YYYY-MM-DD'),
+    # properties
+    'timestamp': arrow.utcnow().timestamp,
+    'datetime': arrow.utcnow().datetime,
+    'year': arrow.utcnow().year,
+    'month': arrow.utcnow().month,
+    'day': arrow.utcnow().day,
+    # mutate
+    'shift': arrow.utcnow().shift(hours=-1),
+    'replace': arrow.utcnow().replace(hour=16),
+    'floor': arrow.utcnow().floor('year'),
+    'ceil': arrow.utcnow().ceil('year'),
+    # timezones
+    'to': arrow.utcnow().to('US/Pacific'),
+    'humanize': arrow.utcnow().humanize(),
+    'humanize_with_locale': arrow.utcnow().humanize(locale='is'),
+}
 
 print(
-    f"now: {now}",
-    f"formatted_now: {formatted_now}",
-    f"first_day_of_year: {first_day_of_year}",
-    # f"dates: {dates}",
-    sep="\n"
+    'basic examples',
+    json.dumps(examples, default=str, indent=2),
+    sep='\n'
 )
 
-# arrow generator basics
+
+# ---
+# arrow generator basics (range)
+# ---
 
 now = arrow.now()
-first_day_of_year = arrow.now().floor('year')
+first_day_of_year = arrow.now().floor('week')
 dates_generator = arrow.Arrow.range('day', first_day_of_year, now)
+print('generated dates')
 for day in dates_generator:
     print(day.format('YYYY-MM-DD'))
