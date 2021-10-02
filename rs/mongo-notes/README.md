ea0d6daa0cb98e18306ac6b02e9b3db7f29ffbff
Landon Yarrington <jly.dev.36963@gmail.com>
2021-10-01 21:19:51 -0600

    rust mongo

--- rs/mongo-notes/README.md
+++ rs/mongo-notes/README.md
@@ -1,6 +1,8 @@
 # mongodb
 
 - https://github.com/mongodb/mongo-rust-driver
+- https://docs.mongodb.com/drivers/rust/
+- https://docs.rs/mongodb/2.0.0/mongodb/
 
 ## Startup
 
--- rs/mongo-notes/src/main.rs
+++ rs/mongo-notes/src/main.rs
@@ -1,5 +1,9 @@
 use async_trait::async_trait;
-use mongodb::{bson::doc, options::ClientOptions, Client};
+use mongodb::{
+    bson::{doc, Bson},
+    options::{ClientOptions, FindOneAndUpdateOptions},
+    Client,
+};
 use serde::{Deserialize, Serialize};
 
 // ---
@@ -9,7 +13,7 @@ use serde::{Deserialize, Serialize};
 #[tokio::main]
 async fn main() {
     // Connection
-    let mongo_conn = String::from("mongodb://localhost:27017");
+    let mongo_conn = String::from("mongodb://root:example@localhost:27017");
     let client_options = ClientOptions::parse(mongo_conn).await.unwrap();
     let client = Client::with_options(client_options).unwrap();
     let db = client.database("practice");
@@ -21,34 +25,35 @@ async fn main() {
     // ---
 
     // Insert one
+    println!("insert_one example");
     let ninja_new = NinjaNew {
         first_name: "Kakashi".to_string(),
         last_name: "Hatake".to_string(),
         age: 27,
     };
     let result = mongo_dal.create_ninja(ninja_new).await;
-    match result {
-        Ok(r) => match r {
-            Some(n) => println!("Ninja insert_one result: {:#?}", n),
-            None => println!("Could not find newly inserted ninja"),
-        },
-        Err(e) => println!("Ninja insert_one error: {:#?}", e),
-    };
+    let _id = result.unwrap().unwrap()._id;
+    println!("Ninja insert_one result: {}", _id);
+
     // Find one
-    let id = result.unwrap().unwrap().id;
-    let result = mongo_dal.get_ninja_by_id(id).await;
-    match result {
-        Ok(r) => match r {
-            Some(n) => println!("Ninja find_one result: {:#?}", n),
-            None => println!("Could not find ninja with id: {}", id),
-        },
-        Err(e) => println!("Ninja find_one error: {:#?}", e),
-    };
+    println!("find_one example");
+    let result = mongo_dal.get_ninja_by_id(_id).await;
+    let ninja = result.unwrap().unwrap();
+    println!("Ninja find_one result: {:#?}", ninja);
+
     // Update one
-    // TODO
+    println!("find_one_and_update example");
+    let result = mongo_dal
+        .update_ninja_last_name(ninja._id, "Sensei".to_string())
+        .await;
+    let ninja = result.unwrap().unwrap();
+    println!("Ninja find_one_and_update result: {:#?}", ninja);
 
     // Delete one
-    // TODO
+    println!("find_one_and_delete example");
+    let result = mongo_dal.delete_ninja(ninja._id).await;
+    let ninja = result.unwrap().unwrap();
+    println!("Ninja delete result: {:#?}", ninja);
 
     // Insert many
     // TODO
@@ -80,7 +85,13 @@ struct MongoDAL {
 #[async_trait]
 pub trait TMongoDAL {
     async fn create_ninja(&self, ninja: NinjaNew) -> Result<Option<Ninja>, mongodb::error::Error>;
-    async fn get_ninja_by_id(&self, id: String) -> Result<Option<Ninja>, mongodb::error::Error>;
+    async fn get_ninja_by_id(&self, _id: Bson) -> Result<Option<Ninja>, mongodb::error::Error>;
+    async fn update_ninja_last_name(
+        &self,
+        _id: Bson,
+        last_name: String,
+    ) -> Result<Option<Ninja>, mongodb::error::Error>;
+    async fn delete_ninja(&self, _id: Bson) -> Result<Option<Ninja>, mongodb::error::Error>;
 }
 
 #[async_trait]
@@ -96,19 +107,45 @@ impl TMongoDAL for MongoDAL {
             .insert_one(ninja_new, None)
             .await?;
         // Get newly inserted ninja (TODO: combine into single operation?)
-        let id = result.inserted_id;
+        let _id = result.inserted_id;
         let result = self
             .db
             .collection::<Ninja>("ninjas")
-            .find_one(doc! { id }, None)
+            .find_one(doc! { "_id": _id }, None)
             .await;
         result
     }
-    async fn get_ninja_by_id(&self, id: String) -> Result<Option<Ninja>, mongodb::error::Error> {
+    async fn get_ninja_by_id(&self, _id: Bson) -> Result<Option<Ninja>, mongodb::error::Error> {
         let result = self
             .db
             .collection::<Ninja>("ninjas")
-            .find_one(doc! { id }, None)
+            .find_one(doc! { "_id": _id }, None)
+            .await;
+        result
+    }
+    async fn update_ninja_last_name(
+        &self,
+        _id: Bson,
+        last_name: String,
+    ) -> Result<Option<Ninja>, mongodb::error::Error> {
+        let result = self
+            .db
+            .collection::<Ninja>("ninjas")
+            .find_one_and_update(
+                doc! { "_id": _id },
+                doc! { "$set": { "last_name": last_name }},
+                FindOneAndUpdateOptions::builder()
+                    .return_document(mongodb::options::ReturnDocument::After)
+                    .build(),
+            )
+            .await;
+        result
+    }
+    async fn delete_ninja(&self, _id: Bson) -> Result<Option<Ninja>, mongodb::error::Error> {
+        let result = self
+            .db
+            .collection::<Ninja>("ninjas")
+            .find_one_and_delete(doc! { "_id": _id }, None)
             .await;
         result
     }
@@ -127,8 +164,15 @@ pub struct NinjaNew {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Ninja {
-    id: String,
+    _id: mongodb::bson::Bson,
     first_name: String,
     last_name: String,
     age: i32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct NinjaUpdates {
+    first_name: Option<String>,
+    last_name: Option<String>,
+    age: Option<i32>,
 }
