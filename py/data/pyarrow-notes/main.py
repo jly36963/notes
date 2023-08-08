27d489d254a2de78a7f159bea4281a0c70301df9
Landon Yarrington <jly.dev.36963@gmail.com>
2023-08-07 18:03:08 -0600

    Updates

--- py/data/pyarrow-notes/main.py
+++ py/data/pyarrow-notes/main.py
@@ -8,6 +8,15 @@ import numpy as np
 
 
 # ---
+# Notes
+# ---
+
+# Pyarrow has no typing support
+# Everything returns 'unknown' type
+# Attributes of well-defined classes are 'unknown'
+# :(
+
+# ---
 # Constants
 # ---
 
@@ -32,6 +41,12 @@ def main():
     print_section_title('basic array ejection')
     basic_array_ejection()
 
+    print_section_title('basic array compute')
+    basic_array_compute()
+
+    print_section_title('basic array methods')
+    basic_array_methods()
+
     # Tables
     print_section_title('basic table creation')
     basic_table_creation()
@@ -42,14 +57,36 @@ def main():
     print_section_title('basic table read/write parquet')
     basic_table_read_write_parquet()
 
+    print_section_title('basic table details')
+    basic_table_details()
+
+
+# ---
+# Utils
+# ---
 
 def print_section_title(string: str) -> None:
     print(f'\n{string.upper()}\n')
 
 
+def map_res(val):
+    """Map type to more print-friendly type"""
+    if isinstance(val, (pa.Table, pa.Array, pa.ChunkedArray)):
+        return val.to_pylist()
+    if isinstance(val, pa.Scalar):
+        return val.as_py()
+    return val
+
+
+def pretty_print_result_map(results: dict) -> None:
+    """Convert values to more print-friendly types, then print"""
+    print(json.dumps({k: map_res(v) for k, v in results.items()}, indent=2))
+
+
 # ---
 # Setup
 # ---
+
 
 def setup():
     """Initia setup before running examples"""
@@ -85,22 +122,114 @@ def basic_array_ejection():
     list_: List[dict] = arr.to_pylist()
     str_: str = arr.to_string()
 
-    print(json.dumps({
+    results = {
         'to_pandas > to_list': srs.to_list(),
         'to_numpy > tolist': nparr.tolist(),
         'to_pylist': list_,
         'to_string': str_,
-    }, indent=2))
+    }
+
+    pretty_print_result_map(results)
+
+
+def basic_array_compute():
+    """Examples of pa.compute"""
+    arr: pa.Array = pa.array([1, 2, 3, 4, 5], type=pa.int8())
+
+    results: Dict[str, pa.Array | str] = {
+        'arr': arr,
+
+        # Arithmetic
+        'abs': pa.compute.abs(arr),
+        'add': pa.compute.add(arr, 2),
+        'divide': pa.compute.divide(arr, 2),
+        'negate': pa.compute.negate(arr),
+        'power': pa.compute.power(arr, 2),
+        'sqrt': pa.compute.sqrt(arr),
+        'subtract': pa.compute.subtract(arr, 1),
+
+        # Comparison
+        'equal': pa.compute.equal(arr, 3),
+        'greater': pa.compute.greater(arr, 0),
+        'greater_equal': pa.compute.greater_equal(arr, 0),
+        'less': pa.compute.less(arr, 10),
+        'less_equal': pa.compute.less_equal(arr, 10),
+
+        # Aggregations
+        'all': pa.compute.all(pa.compute.greater(arr, 3)),
+        'any': pa.compute.any(pa.compute.greater(arr, 3)),
+        'count': pa.compute.count(arr),
+        'max': pa.compute.max(arr),
+        'mean': pa.compute.mean(arr),
+        'min': pa.compute.min(arr),
+        'mode': pa.compute.mode(arr),
+        'product': pa.compute.product(arr),
+        'stddev': pa.compute.stddev(arr),
+        'sum': pa.compute.sum(arr),
+        'variance': pa.compute.variance(arr),
+    }
+
+    pretty_print_result_map(results)
+
+    # TODO
+
+    # Rounding
+    # ceil, floor, round, round_to_multiple
+
+    # Logs
+    # ln, log10, log2, logb
+
+    # Logical
+    # and_, and_not, invert, or_, xor
+
+    # String predicate
+    # ascii_is_alnum, ascii_is_alpha, ascii_is_alnum, ascii_is_lower
+    # ascii_is_lower, ascii_is_space, ascii_is_title, ascii_is_upper
+    # (utf8 equivalents of above)
+
+    # String transforms
+    # ascii_capitalize, ascii_lower, ascii_reverse, ascii_title, ascii_upper
+    # (utf8 equivalents of above)
+    # replace_substring, replace_substring_regex, ascii_
+
+    # String padding
+    # String trimming
+    # String splitting
+    # Containment Tests
+
+    # Categorizations
+    # is_finite, is_inf, is_nan, is_null, is_valid
+
+    # Selecting / Multiplexing
+    # case_wen, choose, coalesce, if_else
+
+    # Temporal
+    # ...
+
+    # Associative transforms
+    # unique, value_counts
+
+    # Selections
+    # drop_null, filter
+
+    # Structural transforms
+    # fill_null (forward/backward), list_flatten, list_slice, map_lookup
 
 
 def basic_array_methods():
-    print('TODO')
-    # TODO
-    # Select: filter, slice, sort, take, view
-    # Transform: cast,
-    # Nulls: drop_null, fill_null, is_nan, is_null, is_valid
-    # Compare: diff, equals
-    # Agg: sum
+    arr: pa.Array = pa.array([1, 2, 3, 4, 5], type=pa.int8())
+
+    results: dict = {
+        'filter': arr.filter(pa.compute.greater(arr, 3)),
+        'slice': arr.slice(0, 3),  # offset, length
+        'sort': arr.sort(order='descending'),
+        'unique': arr.unique(),
+        'cast': arr.cast(target_type=pa.float64()),
+        'is_valid': arr.is_valid(),  # Also: drop_null, fill_null, is_nan, is_null
+
+    }
+
+    pretty_print_result_map(results)
 
 # ---
 # Table examples
@@ -111,34 +240,27 @@ def basic_array_methods():
 
 def basic_table_creation():
     """Create a table in a variety of ways"""
-    print('From pyarrow arrays')
-    first_names = pa.array(['Kakashi', 'Itachi', 'Shisui'], type=pa.string())
-    last_names = pa.array(['Hatake', 'Uchiha', 'Uchiha'], type=pa.string())
-    tbl: pa.Table = pa.table([first_names, last_names], names=['first_name', 'last_name'])
-    print(tbl)
-
-    print('From List[dict]')
-    tbl: pa.Table = pa.Table.from_pylist([
-        {'first_name': 'Kakashi', 'last_name': 'Hatake', },
-        {'first_name': 'Itachi', 'last_name': 'Uchiha', },
-        {'first_name': 'Shisui', 'last_name': 'Uchiha', },
-    ])
-    print(tbl)
-
-    print('From Dict[str, list]')
-    tbl: pa.Table = pa.Table.from_pydict({
-        'first_name': ['Kakashi', 'Itachi', 'Shisui'],
-        'last_name': ['Hatake', 'Uchiha', 'Uchiha'],
-    })
-    print(tbl)
-
-    print('From pandas df')
-    tbl = pa.Table.from_pandas(pd.DataFrame([
-        {'first_name': 'Kakashi', 'last_name': 'Hatake', },
-        {'first_name': 'Itachi', 'last_name': 'Uchiha', },
-        {'first_name': 'Shisui', 'last_name': 'Uchiha', },
-    ]))
-    print(tbl)
+    results = {
+        'From pyarrow arrays':  pa.table([
+            pa.array(['Kakashi', 'Itachi', 'Shisui'], type=pa.string()),
+            pa.array(['Hatake', 'Uchiha', 'Uchiha'], type=pa.string())
+        ], names=['first_name', 'last_name']),
+        'From List[dict]': pa.Table.from_pylist([
+            {'first_name': 'Kakashi', 'last_name': 'Hatake', },
+            {'first_name': 'Itachi', 'last_name': 'Uchiha', },
+            {'first_name': 'Shisui', 'last_name': 'Uchiha', },
+        ]),
+        'From Dict[str, list]': pa.Table.from_pydict({
+            'first_name': ['Kakashi', 'Itachi', 'Shisui'],
+            'last_name': ['Hatake', 'Uchiha', 'Uchiha'],
+        }),
+        'From pandas df': pa.Table.from_pandas(pd.DataFrame([
+            {'first_name': 'Kakashi', 'last_name': 'Hatake', },
+            {'first_name': 'Itachi', 'last_name': 'Uchiha', },
+            {'first_name': 'Shisui', 'last_name': 'Uchiha', },
+        ])),
+    }
+    pretty_print_result_map(results)
 
 
 def basic_table_eject():
@@ -149,54 +271,52 @@ def basic_table_eject():
         {'first_name': 'Shisui', 'last_name': 'Uchiha', },
     ])
 
-    df: pd.DataFrame = tbl.to_pandas()  # NOTE: Requires pandas installation
-    dict_: Dict[str, list] = tbl.to_pydict()
-    list_: List[dict] = tbl.to_pylist()
-    str_: str = tbl.to_string()
+    results = {
+        # NOTE: Requires pandas installation (to_pandas)
+        'to_pandas > to_dict': tbl.to_pandas().to_dict(orient='records'),
+        'to_pydict': tbl.to_pydict(),  # Dict[str, list]
+        'to_pylist': tbl.to_pylist(),  # List[dict]
+        'to_string': tbl.to_string(),  # str
+    }
 
-    print(json.dumps({
-        'to_pandas > to_dict': df.to_dict(orient='records'),
-        'to_pydict': dict_,
-        'to_pylist': list_,
-        'to_string': str_,
-    }, indent=2))
+    pretty_print_result_map(results)
 
 
 def basic_table_read_write_parquet():
     """Write to parquet file and read back into pyarrow table"""
     numbers = pa.table([
-        pa.array(range(1, 10), type=pa.int8()),
-        pa.array(range(10, 100, 10), type=pa.int8()),
-        pa.array(range(100, 1000, 100), type=pa.int16()),
+        pa.array(range(1, 6), type=pa.int8()),
+        pa.array(range(10, 60, 10), type=pa.int8()),
+        pa.array(range(100, 600, 100), type=pa.int16()),
     ], names=['a', 'b', 'c'])
 
     fn = 'numbers.parquet'
     fp = os.path.join(DATA_OUTPUT_DIR, fn)
 
-    print('writing table')
     pq.write_table(numbers, fp)
-    print('reading table')
     tbl = pq.read_table(fp)
-    print(tbl)
+    print(tbl.to_pydict())
 
 
 def basic_table_details():
-    print('TODO')
+    """Get basic details about the table"""
     tbl: pa.table = pa.Table.from_pylist([
         {'first_name': 'Kakashi', 'last_name': 'Hatake', },
         {'first_name': 'Itachi', 'last_name': 'Uchiha', },
         {'first_name': 'Shisui', 'last_name': 'Uchiha', },
     ])
 
-    print({
+    results = {
         'column_names': tbl.column_names,
-        'columns': tbl.columns,
+        'columns > map > combine_chunks > to_pylist': [col.combine_chunks().to_pylist() for col in tbl.columns],
         'nbytes': tbl.nbytes,
         'num_columns': tbl.num_columns,
         'num_rows': tbl.num_rows,
         'schema': tbl.schema,
         'shape': tbl.shape,
-    })
+    }
+
+    print(results)
 
 
 def basic_table_methods():
