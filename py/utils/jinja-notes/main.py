9537264fb6ba1638e2ea278435f6f783f5e8d430
Landon Yarrington <jly.dev.36963@gmail.com>
2023-09-05 07:58:32 -0600

    jinja

---
+++ py/utils/jinja-notes/main.py
@@ -0,0 +1,131 @@
+from datetime import datetime, timedelta
+import json
+from typing import Any, Dict
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+# TODO: macro/call, filter, extends, include
+
+
+def main():
+    env = Environment(
+        loader=PackageLoader("main"),
+        autoescape=select_autoescape()
+    )
+
+    print_section_title("Interpolation")
+    basic_interpolation(env)
+
+    print_section_title("Fallback")
+    basic_fallback(env)
+
+    print_section_title("Looping")
+    basic_looping(env)
+
+    print_section_title("If")
+    basic_if(env)
+
+    print_section_title("Ternary")
+    basic_ternary(env)
+
+
+# ---
+# Utils
+# ---
+
+
+def print_section_title(string: str) -> None:
+    """Wrap with newlines, convert to uppercase, print"""
+    print(f'\n{string.upper()}\n')
+
+
+def map_res(val: Any) -> Any:
+    """Map type to more print-friendly type"""
+    # if isinstance(val, DataFrame):
+    #     return df_to_records(val)
+    return val
+
+
+def pretty_print_result_map(results: Dict[str, Any]) -> None:
+    """Convert values to more print-friendly types, then pretty print"""
+    print(json.dumps({k: map_res(v) for k, v in results.items()}, indent=2))
+
+
+# ---
+# Examples
+# ---
+
+def basic_get_template(env: Environment) -> None:
+    """Get template from file"""
+    # Find template at './templates/template.md'
+    template = env.get_template("template.md")
+    print(template.render(phrase="Finland!"))
+
+
+INTERPOLATION_TEMPLATE = 'Hello there {{ name }}'
+
+
+def basic_interpolation(env: Environment) -> None:
+    """Interpolation example"""
+    template = env.from_string(INTERPOLATION_TEMPLATE)
+    print(template.render(name="Kakashi"))
+
+
+FALLBACK_TEMPLATE = "Hello there {{ name or 'friend' }}"
+
+
+def basic_fallback(env: Environment) -> None:
+    """Fallback example"""
+    template = env.from_string(FALLBACK_TEMPLATE)
+    print(template.render())
+
+
+LOOPING_TEMPLATE = '''
+Numbers:
+{% for number in numbers %}
+- {{ number }}
+{% endfor %}
+'''.strip()
+
+
+def basic_looping(env: Environment) -> None:
+    """Loop example"""
+    template = env.from_string(LOOPING_TEMPLATE)
+    print(template.render(numbers=[1, 2, 3, 4, 5]))
+
+
+IF_TEMPLATE = '''
+{% if is_morning %}
+Have a great morning!
+{% else %}
+Have a great day!
+{% endif %}
+'''.strip()
+
+
+def basic_if(env: Environment) -> None:
+    """If example"""
+    dt = datetime.now()
+    is_morning = dt.hour < 12
+    template = env.from_string(IF_TEMPLATE)
+    print(template.render(is_morning=is_morning))
+
+
+TERNARY_TEMPLATE = '''
+Have a great {{ 'morning' if is_morning else 'day' }}!
+'''.strip()
+
+
+def basic_ternary(env: Environment) -> None:
+    """If example"""
+    dt = datetime.now()
+    is_morning = dt.hour < 12
+    template = env.from_string(TERNARY_TEMPLATE)
+    print(template.render(is_morning=is_morning))
+
+
+# ---
+# Run
+# ---
+
+if __name__ == '__main__':
+    main()
---
+++ py/utils/jinja-notes/templates/template.md
@@ -0,0 +1,3 @@
+# Get template
+
+Patrick says "{{ phrase }}"
